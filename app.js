5614cba55fceb81fc558d1d86c25da3098ac7693
MatthewDodi <matthew.dodi@gmail.com>
2018-09-24 19:38:54 -0700

    using require() to load in modules

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello Node!');
+console.log('Starting app.');
+
+const fs = require('fs');
+const os = require('os');
+
+let user = os.userInfo();
+
+fs.appendFileSync('Greetings.txt', ` you must be ${user.username}, right?`);
+
